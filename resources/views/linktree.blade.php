963edd1e5b69ed9f11bea3a5c291cb5ee323bc57
nadciel <grace.b1645@gmail.com>
2025-05-14 12:21:13 +0700

    pertemuan 11

---
+++ resources/views/linktree.blade.php
@@ -0,0 +1,201 @@
+<html>
+
+<head>
+    <title>ENSEMBLE STARS | Linktree</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+        body {
+            background-color: #373735;
+            font-family: "DM Sans";
+            height: 100%;
+            width: 100%;
+        }
+
+        .container {
+            width: 100%;
+            height: 100%;
+            padding-right: 15px;
+            padding-left: 15px;
+            margin-right: auto;
+            margin-left: auto;
+        }
+
+        @media (max-width: 480px) {
+            .container {
+                max-width: 100%;
+            }
+        }
+
+        .link-button {
+            gap: 20px;
+        }
+
+        .link-button p {
+            flex: 1;
+            text-align: center;
+            font-weight: 500;
+            font-size: 16px;
+            overflow-wrap: anywhere;
+        }
+
+        .link-button a {
+            width: 100%;
+        }
+
+        .button:hover {
+            background-color: transparent;
+            border: 2px solid white;
+            color: white;
+        }
+
+        .bi-three-dots-vertical:hover {
+            background-color: #4b4c4a;
+            border-radius: 50%;
+            padding: 2px;
+            color: white !important;
+        }
+
+        .button-footer:hover {
+            background-color: white !important;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container d-flex flex-column align-items-center">
+        <div class="d-flex flex-column justify-content-center pb-5 col-xl-4 col-lg-6 col-md-8 col-12"
+            style="max-width: 580px;">
+            <div class="d-flex justify-content-end align-items-right pt-4">
+                <button type="button" class="btn rounded-circle text-white mt-1"
+                    style="background-color: #2d2d2b; width: 40px; height: 40px;">
+                    <i class="bi bi-three-dots"></i>
+                </button>
+            </div>
+
+            <div class="d-flex flex-column justify-content-center align-items-center rounded-circle mb-4">
+                <img src="asset/profile.png" class="rounded-circle"
+                    style="width: 96px; height: 96px; border: 10px solid white;">
+                <h5 class="font-weight-bold text-center text-white mt-3">ENSEMBLE STARS</h5>
+            </div>
+
+            <div class="d-flex flex-column justify-content-center align-items-center" style="min-width: 100%;">
+                <a href="https://www.tumblr.com/machifuwa/713863068149514240/0406-tsukasa-suou"
+                    class="btn btn-light btn-lg btn-block border-white py-1 my-2 button text-decoration-none"
+                    target="_blank" style="max-width: 580px; overflow-wrap: anywhere; border-radius: 29px;">
+                    <div class="row link-button justify-content-between my-1 px-2" style="overflow-wrap: break-word;">
+                        <img src="asset/tumblr_4fa15ec0688c4069d2479f571da19e20_0fe175f9_1280.jpg" width="46px"
+                            height="46px" class="rounded-circle my-auto">
+                        <p class="text-wrap my-2" style="font-size: 16px; font-weight: 500;">[04.06.23] Tsukasa
+                            Suou</p>
+                        <i class="bi bi-three-dots-vertical my-auto" style="color: darkgray;"></i>
+                    </div>
+                </a>
+
+                <a href="https://www.tumblr.com/machifuwa/687915134196334592/bankara-graffiti-we-who-dont-learn"
+                    class="btn btn-light btn-lg btn-block border-white py-1 my-2 button text-decoration-none"
+                    target="_blank" style="max-width: 580px; overflow-wrap: anywhere; border-radius: 29px;">
+                    <div class="row link-button justify-content-between my-1 px-2" style="overflow-wrap: break-word;">
+                        <img src="asset/tumblr_0c1017da799c66f119ec4896134e3d5b_0585fb74_2048.jpg" width="46px"
+                            height="46px" class="rounded-circle my-auto">
+                        <p class="text-wrap my-2" style="font-size: 16px; font-weight: 500;">【Bankara Graffiti】 We
+                            Who Don't Learn</p>
+                        <i class="bi bi-three-dots-vertical my-auto" style="color: darkgray;"></i>
+                    </div>
+                </a>
+
+                <a href="https://www.tumblr.com/machifuwa/689640370701746176/mizutama-pattern-youth-is-the-color-of-summer"
+                    class="btn btn-light btn-lg btn-block border-white py-1 my-2 button text-decoration-none"
+                    target="_blank" style="max-width: 580px; overflow-wrap: anywhere; border-radius: 29px;">
+                    <div class="row link-button justify-content-between my-1 px-2" style="overflow-wrap: break-word;">
+                        <img src="asset/e2eda7da7d926a927380f29569056e5a.jpg" width="46px" height="46px"
+                            class="rounded-circle my-auto">
+                        <p class="text-wrap my-2" style="font-size: 16px; font-weight: 500;">【Mizutama Pattern】 Youth
+                            is the Color of Summer</p>
+                        <i class="bi bi-three-dots-vertical my-auto" style="color: darkgray;"></i>
+                    </div>
+                </a>
+
+                <a href="https://www.tumblr.com/machifuwa/695352871064076288/mirage-worlds-most-beautiful-mirror?source=share"
+                    class="btn btn-light btn-lg btn-block border-white py-1 my-2 button text-decoration-none"
+                    target="_blank" style="max-width: 580px; overflow-wrap: anywhere; border-radius: 29px;">
+                    <div class="row link-button justify-content-between my-1 px-2" style="overflow-wrap: break-word;">
+                        <img src="asset/29_Yuta_Aoi_CG.jpg" width="46px" height="46px" class="rounded-circle my-auto">
+                        <p class="text-wrap my-2" style="font-size: 16px; font-weight: 500;">【MIRAGE】 World's Most
+                            Beautiful Mirror</p>
+                        <i class="bi bi-three-dots-vertical my-auto" style="color: darkgray;"></i>
+                    </div>
+                </a>
+
+                <a href="https://www.tumblr.com/machifuwa/706961480887713792/nimbus-fumble"
+                    class="btn btn-light btn-lg btn-block border-white py-1 my-2 button text-decoration-none"
+                    target="_blank" style="max-width: 580px; overflow-wrap: anywhere; border-radius: 29px;">
+                    <div class="row link-button justify-content-between my-1 px-2" style="overflow-wrap: break-word;">
+                        <img src="asset/tumblr_f97565039112b4dffac6c8c81bdf6ef8_b2d68080_2048.jpg" width="46px"
+                            height="46px" class="rounded-circle my-auto">
+                        <p class="text-wrap my-2" style="font-size: 16px; font-weight: 500;">【Nimbus FUMBLE】</p>
+                        <i class="bi bi-three-dots-vertical my-auto" style="color: darkgray;"></i>
+                    </div>
+                </a>
+
+                <a href="https://www.tumblr.com/machifuwa/689031190899015681/keitos-lessons-summer-splash"
+                    class="btn btn-light btn-lg btn-block border-white py-1 my-2 button text-decoration-none"
+                    target="_blank" style="max-width: 580px; overflow-wrap: anywhere; border-radius: 29px;">
+                    <div class="row link-button justify-content-between my-1 px-2" style="overflow-wrap: break-word;">
+                        <img src="asset/089d1a04b23f207c46ab10783609b6c7.jpg" width="46px" height="46px"
+                            class="rounded-circle my-auto">
+                        <p class="text-wrap my-2" style="font-size: 16px; font-weight: 500;">【Keito's Lessons】 Summer
+                            Splash</p>
+                        <i class="bi bi-three-dots-vertical my-auto" style="color: darkgray;"></i>
+                    </div>
+                </a>
+
+                <a href="https://www.tumblr.com/machifuwa/691000751198240768/animals-menagerie-of-animals"
+                    class="btn btn-light btn-lg btn-block border-white py-1 my-2 button text-decoration-none"
+                    target="_blank" style="max-width: 580px; overflow-wrap: anywhere; border-radius: 29px;">
+                    <div class="row link-button justify-content-between my-1 px-2" style="overflow-wrap: break-word;">
+                        <img src="asset/06cac6fb22dd17822b3072d5d9bf65c9.jpg" width="46px" height="46px"
+                            class="rounded-circle my-auto">
+                        <p class="text-wrap my-2" style="font-size: 16px; font-weight: 500;">【Animals】 Menagerie of
+                            Animals</p>
+                        <i class="bi bi-three-dots-vertical my-auto" style="color: darkgray;"></i>
+                    </div>
+                </a>
+
+                <a href="https://www.tumblr.com/machifuwa/690997687954325504/summer-breeze-sunshine-shimmering-in-a-foreign"
+                    class="btn btn-light btn-lg btn-block border-white py-1 my-2 button text-decoration-none"
+                    target="_blank" style="max-width: 580px; overflow-wrap: anywhere; border-radius: 29px;">
+                    <div class="row link-button justify-content-between my-1 px-2" style="overflow-wrap: break-word;">
+                        <img src="asset/ee77eec11901508c78ff5ab8d48d1c74.jpg" width="46px" height="46px"
+                            class="rounded-circle my-auto">
+                        <p class="text-wrap my-2" style="font-size: 16px; font-weight: 500;">【Summer Breeze!】
+                            Sunshine Shimmering in a Foreign Land</p>
+                        <i class="bi bi-three-dots-vertical my-auto" style="color: darkgray;"></i>
+                    </div>
+                </a>
+            </div>
+
+            <div class="d-flex flex-column justify-content-center align-items-center my-4">
+                <button type="button" class="btn btn-lg btn-block rounded-pill justify-content-between button-footer"
+                    style="width: fit-content; max-width: 100%; background-color: #e1e1e1;">
+                    <div class="row link-button justify-content-between align-items-center px-2">
+                        <img src="asset/linktree_logo_icon_247832.png" class="ml-2" style="height: 16px;">
+                        <p class="text-wrap my-1 mx-2" style="font-family: Arial; font-size: medium; font-weight: 600; color: black;">Join machifuwa on Linktree</p>
+                    </div>
+                </button>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
