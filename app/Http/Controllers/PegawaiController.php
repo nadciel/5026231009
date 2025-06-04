0d43c6b5944b26b03a868e22f36e12fb6a6bfae7
Bernadetta Graciela <162095165+nadciel@users.noreply.github.com>
2025-06-04 12:25:25 +0700

    pertemuan 14

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -11,11 +11,11 @@ class PegawaiController extends Controller
     public function index()
 	{
     	// mengambil data dari table pegawai
-		$pegawai = DB::table('pegawai')->get();
+		// $pegawai = DB::table('pegawai')->get();
+		$pegawai = DB::table('pegawai')->paginate(10);
  
     	// mengirim data pegawai ke view index
 		return view('index',['pegawai' => $pegawai]);
- 
 	}
  
 	// method untuk menampilkan view form tambah pegawai
@@ -24,7 +24,6 @@ class PegawaiController extends Controller
  
 		// memanggil view tambah
 		return view('tambah');
- 
 	}
  
 	// method untuk insert data ke table pegawai
@@ -39,7 +38,6 @@ class PegawaiController extends Controller
 		]);
 		// alihkan halaman ke halaman pegawai
 		return redirect('/pegawai');
- 
 	}
  
 	// method untuk edit data pegawai
@@ -49,7 +47,6 @@ class PegawaiController extends Controller
 		$pegawai = DB::table('pegawai')->where('pegawai_id',$id)->get();
 		// passing data pegawai yang didapat ke view edit.blade.php
 		return view('edit',['pegawai' => $pegawai]);
- 
 	}
  
 	// update data pegawai
@@ -74,5 +71,20 @@ class PegawaiController extends Controller
 		
 		// alihkan halaman ke halaman pegawai
 		return redirect('/pegawai');
+	}
+
+	// method untuk mencari pegawai
+ 	public function cari(Request $request)
+	{
+		// menangkap data pencarian
+		$cari = $request->cari;
+ 
+    		// mengambil data dari table pegawai sesuai pencarian data
+		$pegawai = DB::table('pegawai')
+		->where('pegawai_nama','like',"%".$cari."%")
+		->paginate();
+ 
+    		// mengirim data pegawai ke view index
+		return view('index',['pegawai' => $pegawai, 'cari' => $cari]);
 	}
 }
--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -1,26 +1,51 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
-</head>
-<body>
- 
-	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
+@extends('template')
+
+@section('content')
 	<h3>Data Pegawai</h3>
  
-	<a href="/pegawai"> Kembali</a>
+	<a href="/pegawai" class="btn btn-info"> Kembali</a>
 	
 	<br/>
 	<br/>
  
 	<form action="/pegawai/store" method="post">
 		{{ csrf_field() }}
-		Nama <input type="text" name="nama" required="required"> <br/>
-		Jabatan <input type="text" name="jabatan" required="required"> <br/>
-		Umur <input type="number" name="umur" required="required"> <br/>
-		Alamat <textarea name="alamat" required="required"></textarea> <br/>
-		<input type="submit" value="Simpan Data">
+		<div class="row my-2">
+			<div class="col-3">
+				Nama
+			</div>
+			<div class="col-8">
+				<input type="text" name="nama" required="required" class="form-control">
+			</div>
+		</div>
+		<div class="row my-2">
+			<div class="col-3">
+				Jabatan
+			</div>
+			<div class="col-8">
+				<input type="text" name="jabatan" required="required"  class="form-control">
+			</div>
+		</div>
+		<div class="row my-2">
+			<div class="col-3">
+				Umur
+			</div>
+			<div class="col-8">
+				<input type="number" name="umur" required="required" class="form-control">
+			</div>
+		</div>
+		<div class="row my-2">
+			<div class="col-3">
+				Alamat
+			</div>
+			<div class="col-8">
+				<textarea name="alamat" required="required" class="form-control"></textarea>
+			</div>
+		</div>
+		<div class="row my-2">
+			<div class="col-3">
+				<input type="submit" value="Simpan Data" class="btn btn-success">
+			</div>
+		</div>
 	</form>
- 
-</body>
-</html>
+@endsection
